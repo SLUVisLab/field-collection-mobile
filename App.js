a5fb15210ea9e1fc89050ad4492cb5a6440f4870
Alex Juan <juanaw@hopper.slu.edu>
2021-09-30 15:53:25 -0500

    webview

--- App.js
+++ App.js
@@ -1,53 +1,41 @@
-import React, { Component } from 'react'
-import {
-  StyleSheet,
-  TouchableOpacity,
-  Text,
-  View,
-} from 'react-native'
+import React from 'react';
+import { StyleSheet, Text, Button, View, Dimensions } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-class App extends Component {
-  state = {
-    count: 0
-  }
+const deviceHeight = Dimensions.get('window').height;
+const deviceWidth = Dimensions.get('window').width;
 
-  onPress = () => {
-    this.setState({
-      count: this.state.count + 1
-    })
-  }
-
- render() {
-    return (
-      <View style={styles.container}>
-        <TouchableOpacity
-         style={styles.button}
-         onPress={this.onPress}
-        >
-         <Text>Click me</Text>
-        </TouchableOpacity>
-        <View>
-          <Text>
-            You clicked { this.state.count } times
-          </Text>
-        </View>
-      </View>
-    )
-  }
+export default function FormView() {
+  return (
+    <View style={{ flex: 1, alignItems: 'flex-end' }}>
+      <WebView 
+      source= {{ uri: 'https://docs.google.com/forms/d/e/1FAIpQLSfIvIoFyEUeNeuH-XpwNBKjojoTINopXElLx8kG95zPR85TiA/viewform?usp=sf_link'}}
+      style={styles.webview}
+      javaScriptEnabled={true}
+      domStorageEnabled={true}
+      startInLoadingState={false}
+      scalesPageToFit={true}
+      />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  button: {
-    alignItems: 'center',
-    backgroundColor: '#DDDDDD',
-    padding: 10,
-    marginBottom: 10
-  }
-})
 
-export default App;
+const styles = StyleSheet.create({
+    pageNatureQuest:{
+      flex:1,
+      flexDirection:'column',
+      alignItems:'center',
+    },
+    headerContainer: {
+      width:'100%',
+      backgroundColor:'#ededed',
+      },
+
+    webview: {
+      flex: 1,
+      backgroundColor: 'yellow',
+      width: deviceWidth,
+      height: deviceHeight
+    }
+  }); 
