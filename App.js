fa5765d8c30b07944c704794dc8abf3666cb7e38
Hamad Khan <hamad.khan@slu.edu>
2021-10-06 03:21:32 -0500

    Add scanning functionality

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, Button, View, Dimensions } from 'react-native';
 import { WebView } from 'react-native-webview';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 const deviceHeight = Dimensions.get('window').height;
 const deviceWidth = Dimensions.get('window').width;
@@ -23,15 +24,41 @@ function HomeScreen({ navigation }) {
   );
 }
 
-function CameraView({ navigation }) {
+function QRCode({ navigation }) {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    // Right now, it just goes to the form view no matter what barcode is scanned
+    navigation.navigate('FormView')
+    // alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text> Camera should be here somewhere. </Text>
+    <View style={styles.container}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
     </View>
   );
 }
-
-
 
 function FormView({ navigation }) {
   return (
@@ -48,7 +75,6 @@ function FormView({ navigation }) {
   );
 }
 
-
 const Stack = createNativeStackNavigator();
 
 function App() {
@@ -57,14 +83,18 @@ function App() {
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="FormView" component={FormView} />
-        <Stack.Screen name="Camera" component={CameraView} />
+        <Stack.Screen name="Camera" component={QRCode} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-
 const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: 'column',
+      justifyContent: 'center',
+    },
     pageNatureQuest:{
       flex:1,
       flexDirection:'column',
