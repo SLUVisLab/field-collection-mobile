70d72f86df544ac9ba94865bb97f6082c95e301a
hamadkhan <hamadkhan@slu.edu>
2021-11-29 15:20:29 -0600

    Updated taskselect and siteselect

--- screens/SiteSelect.js
+++ screens/SiteSelect.js
@@ -1,61 +1,55 @@
-import React, {useState} from 'react';
+import React, { Component } from 'react';
 import { Text, View, ImageBackground } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import styles from '../Styles';
-import {Picker} from '@react-native-picker/picker';
+import { Picker } from '@react-native-picker/picker';
 
-const data = [
-  { id: "", name: "" },
-  { id: "1", name: "Site 1" },
-  { id: "2", name: "Site 2" },
-  { id: "3", name: "Site 3" },
-  { id: "4", name: "Site 4" },
-  { id: "5", name: "Site 5" },
-];
+// For reference: https://stackoverflow.com/questions/43016624/react-native-apply-array-values-from-state-as-picker-items
 
-
-function SiteSelect({ navigation }) {
-  const [selectedSite, setSelectedSite] = useState();
-  return (
-    <View style={styles.container}>   
-      <ImageBackground style={styles.container}
-        source={require('../assets/plantField.jpg')}>
-        <View
-            style={styles.heading}
-          >
+var options = ["Hamad Khan","Julia Pratt","Alexander Juan","Abby Stylianou","Allison Miller"];
+class SiteSelect extends Component {
+   state = {site: ''}
+   updateSite = (site) => {
+      this.setState({ user: site})
+   }
+   render() {
+      return (
+        <View style={styles.container}>   
+          <ImageBackground style={styles.container}
+            source={require('../assets/plantField.jpg')}>
+            <View
+              style={styles.heading}
+            >
             <Text style={styles.textheading}>Please select a site</Text>
-        </View>
+            </View> 
+            <Picker
+            style={styles.picker}
+            selectedValue={this.state.site}
+            onValueChange={this.updateSite}
+            animationType="slide"
+            itemStyle={{ color:"white", fontWeight:"bold", fontSize:20 }}>
+              {options.map((item, index) => {
+                return (< Picker.Item label={item} value={index} key={index} />);
+              })}   
+            </Picker>
+            <View style={{height:10}}></View>
 
-        <Picker
-          style={styles.picker}
-          selectedValue={selectedSite}
-          onValueChange={ (itemValue, itemIndex) => setSelectedSite(itemValue)}
-          animationType="slide"
-          itemStyle={{ color:"white", fontWeight:"bold", fontSize:20 }}
-        >
-          <Picker.Item label="Site 1" value="Site 1" />
-          <Picker.Item label="Site 2" value="Site 2" />
-          <Picker.Item label="Site 3" value="Site 3" />
-          <Picker.Item label="Site 4" value="Site 4" />
-        </Picker>
-        <View style={{height:10}}></View>
+          <TouchableOpacity
+              style={styles.button}
+              onPress={() => this.props.navigation.navigate('TaskSelect', {type: null, selectedSite: this.state.site})}
+            >
+            <Text style={styles.text}>Go</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity
+          <TouchableOpacity
             style={styles.button}
-            onPress={() => navigation.navigate('TaskSelect', {type: null, selectedSite: selectedSite})}
+            onPress={() => this.props.navigation.navigate('Home')}
           >
-          <Text style={styles.text}>Go</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => navigation.navigate('Home')}
-        >
-          <Text style={styles.text}>Back Home</Text>
-        </TouchableOpacity>
-      </ImageBackground>
-    </View>
-  );
+            <Text style={styles.text}>Back Home</Text>
+          </TouchableOpacity>
+          </ImageBackground>
+         </View>
+      )
+   }
 }
-
-export default SiteSelect;
+export default SiteSelect 
--- screens/TaskSelect.js
+++ screens/TaskSelect.js
@@ -1,62 +1,64 @@
-import React, {useState} from 'react';
+import React, { Component } from 'react';
 import { Text, View, ImageBackground } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import styles from '../Styles';
 import {Picker} from '@react-native-picker/picker';
+import { roundToNearestPixel } from 'react-native/Libraries/Utilities/PixelRatio';
 
-const data = [
-  { id: "", name: "" },
-  { id: "1", name: "Task 100" },
-  { id: "2", name: "Task 2" },
-  { id: "3", name: "Task 3" },
-  { id: "4", name: "Task 4" },
-  { id: "5", name: "Task 5" },
-];
+// For reference: https://stackoverflow.com/questions/43016624/react-native-apply-array-values-from-state-as-picker-items
+
+var options ={
+    "https://forms.gle/XV3DD9X7G7fQ2dgC9": "Task 1",
+    "https://forms.gle/XSaLYtu1hP5tGRyN6": "Task 2",
+    "https://forms.gle/82XA66DYpHs6M11w6": "Task 3",
+    "https://forms.gle/EtdCCvVAZMn23osC8": "Task 4",
+};
 
 
-function TaskSelect({ route, navigation }) {
-  const [selectedTask, setSelectedTask] = useState(),
-  params = route.params;
-  return (  
-    <ImageBackground style={styles.container}
-      source={require('../assets/plantField.jpg')}>
-      <View style={styles.container}> 
-        <View
-            style={styles.heading}
-          >
-            <Text style={styles.textheading}>Please select from the list of tasks available at {params.selectedSite}</Text>
-        </View>
+class TaskSelect extends Component {
+   state = {task: ''}
+   updateTask = (task) => {
+      this.setState({ user: task})
+   }
+   render() {
+      const { site } = this.props.route.params;
+      return (
+        <View style={styles.container}>   
+          <ImageBackground style={styles.container}
+            source={require('../assets/plantField.jpg')}>
+            <View
+              style={styles.heading}
+            >
+            <Text style={styles.textheading}>Please select from the list of tasks available at</Text>
+            </View> 
+            <Picker
+            style={styles.picker}
+            selectedValue={this.state.task}
+            onValueChange={this.updateTask}
+            animationType="slide"
+            itemStyle={{ color:"white", fontWeight:"bold", fontSize:20 }}>
+              {Object.keys(options).map((key) => {
+                return (<Picker.Item label={options[key]} value={key} key={key}/>) //if you have a bunch of keys value pair
+              })}
+            </Picker>
+            <View style={{height:10}}></View>
 
-        <Picker
-          style={styles.picker}
-          selectedValue={selectedTask}
-          onValueChange={ (itemValue, itemIndex) => setSelectedTask(itemValue)}
-          animationType="slide"
-          itemStyle={{ color:"white", fontWeight:"bold", fontSize:20 }}
-        >
-          <Picker.Item label="Task 1" value="https://forms.gle/XV3DD9X7G7fQ2dgC9" />
-          <Picker.Item label="Task 2" value="https://forms.gle/XSaLYtu1hP5tGRyN6" />
-          <Picker.Item label="Task 3" value="https://forms.gle/82XA66DYpHs6M11w6" />
-          <Picker.Item label="Task 4" value="https://forms.gle/EtdCCvVAZMn23osC8" />
-        </Picker>
-        <View style={{height:10}}></View>
+          <TouchableOpacity
+              style={styles.button}
+              onPress={() => this.props.navigation.navigate('Camera', {type: null, selectedTask: this.state.task, selectedSite: site})}
+            >
+            <Text style={styles.text}>Scan Plot</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity
+          <TouchableOpacity
             style={styles.button}
-            onPress={() => navigation.navigate('Camera', {type: null, selectedTask: selectedTask, selectedSite: params.selectedSite})}
+            onPress={() => this.props.navigation.navigate('Home')}
           >
-          <Text style={styles.text}>Scan Plot</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => navigation.navigate('Home')}
-        >
           <Text style={styles.text}>Back Home</Text>
-        </TouchableOpacity>
-      </View>
-    </ImageBackground>
-  );
+          </TouchableOpacity>
+          </ImageBackground>
+         </View>
+      )
+   }
 }
-
 export default TaskSelect;
