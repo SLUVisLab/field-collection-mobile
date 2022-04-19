fb735498e354f57c1061c6082ac63a7302cc6689
Hamad Khan <hamad.khan@slu.edu>
2022-04-19 12:57:01 -0500

    Update

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -16,7 +16,7 @@ function HomeScreen({ navigation }) {
           </TouchableOpacity>
           <TouchableOpacity
             style={styles.button}
-            onPress={() => navigation.navigate('FormView', {type: null, data: 'https://docs.google.com/spreadsheets/d/1QxCk3aqvCFH-5X4OWYPcuK7v0Hf4iyc8g6wsUtKklb8/edit#gid=0'})}
+            onPress={() => navigation.navigate('SheetScreen')}
           >
             <Text style={styles.text}>Edit Surveys</Text>
           </TouchableOpacity>
