9227442dd417e8efbdee0412870904a65357fea5
austin c <ac6404@gmail.com>
2024-06-11 13:48:56 -0500

    add firebase storage config

--- firebase.js
+++ firebase.js
@@ -1,23 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = initializeApp({
-  apiKey: "AIzaSyCYrdDMfQ9yaf6OkT7gLLgnsRg1yJoF-2Y",
-  authDomain: "capstone-fire-c5203.firebaseapp.com",
-  projectId: "capstone-fire-c5203",
-  storageBucket: "capstone-fire-c5203.appspot.com",
-  messagingSenderId: "352112241862",
-  appId: "1:352112241862:web:ab47b00cbc34953a6dfbc7",
-  measurementId: "G-YE03EG7Z0Q"
-});
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: "data-collection-44ddc.firebaseapp.com",
+  projectId: "data-collection-44ddc",
+  storageBucket: "data-collection-44ddc.appspot.com",
+  messagingSenderId: "862929951775",
+  appId: "1:862929951775:web:4e2ddbd607cee8e7b8892a"
+};
 
 // Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-var db = getFirestore();
-export default db
+var storage = getStorage();
+export default storage;
