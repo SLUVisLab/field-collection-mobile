6c9b74ca742c81d0e4fb8f097ff32e5bad83bd30
austin c <ac6404@gmail.com>
2025-05-05 06:52:49 -0500

    update realm version

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,10 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require('@expo/metro-config');
 
-module.exports = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
+
+// If you need any customizations, add them here
+// For example:
+// config.resolver.assetExts.push('db');
+
+module.exports = config;
