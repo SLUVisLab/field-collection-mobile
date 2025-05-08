48bc7252cf6b7b310a3ab37e016e02f2598dd3b4
austin c <ac6404@gmail.com>
2025-05-08 15:19:19 -0500

    add config for google maps keys[build]

--- tasks/geopoint/GeoPointAction.js
+++ tasks/geopoint/GeoPointAction.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Modal } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import * as Location from 'expo-location';
 import styles from '../../Styles'; // Your shared styles
 
@@ -52,6 +52,7 @@ const GeoPointAction = ({ existingData, onComplete, task, item, collection }) =>
           <>
             <MapView
               style={{ flex: 1 }}
+              provider={PROVIDER_GOOGLE}
               showsUserLocation
               region={{
                 latitude: currentLocation.latitude,
